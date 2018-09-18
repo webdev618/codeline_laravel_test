3a33e8fb9cab923d98120a3bc8d877755f89b0f3
webdev618 <webdev618@gmail.com>
2018-09-18 04:02:36 -0700

    1.2 DB seeder with 3 films

--- database/factories/FilmFactory.php
+++ database/factories/FilmFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Film::class, function (Faker $faker) {
+    $genres = array();
+    for ($i = 0; $i < $faker->numberBetween(1, 5); $i++) {
+        $genres[] = $faker->word(1);
+    }
     return [
-        //
+        'name' => substr($faker->sentence(2), 0, -1),
+        'description' => $faker->paragraph(6),
+        'release_date' => $faker->dateTimeBetween('-5 years'),
+        'rating' => $faker->numberBetween(1, 5),
+        'ticket_price' => $faker->randomNumber(3),
+        'country' => $faker->country,
+        'genre' => implode(',', $faker->shuffleArray($genres)),
+        'photo' => $faker->imageUrl()
     ];
 });
