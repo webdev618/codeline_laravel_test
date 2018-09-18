faadf92845d530d788a35c0e178d7a5d7159d349
webdev618 <webdev618@gmail.com>
2018-09-18 03:10:47 -0700

    1.1 Backend
    create "films" table

---
+++ database/migrations/2018_09_18_092047_create_films_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->dateTime('release_date');
+            $table->unsignedTinyInteger('rating');
+            $table->integer('ticket_price');
+            $table->string('country');
+            $table->text('genre');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
